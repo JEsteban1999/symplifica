374ed2b3151eb926ce9c9f1ba3c61cc54bfc8deb
Juan Esteban Narváez Carvajal <116776212+JEsteban1999@users.noreply.github.com>
2024-11-25 23:53:00 -0500

    application.properties modified and migrations created

---
+++ src/main/resources/db/migration/V3__Create_order_product_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE order_product (
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    PRIMARY KEY (order_id, product_id),
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
